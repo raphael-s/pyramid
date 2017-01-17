c2ebd0b9aa7401d1f329a53fead63ad616b1494c
raphael-s <r.schwob@4teamwork.ch>
2017-01-17 17:04:25 +0100

    Improve detail view

--- firstpyramid/views/images.py
+++ firstpyramid/views/images.py
@@ -2,14 +2,15 @@ from deform.schema import FileData
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 from resources import ImageItem
-import colander
 from security import b64
+import colander
 import deform
 import deform.widget as w
 import firstpyramid
 
 
 class Storage(dict):
+
     def preview_url(self, name):
         return ""
 
@@ -74,23 +75,44 @@ def upload_view(request):
 @view_config(route_name='images', renderer='../templates/detail.pt')
 def detail_view(request):
     item_id = request.url.split('/')[-1]
+    user = request.authenticated_userid
 
     root = firstpyramid.root_factory(request)
 
     item = root['images'].get(int(item_id))
 
-    return{'item': {'title': item['title'],
-                    'image': b64(item['image']),
-                    'id': item_id,
-                    'uploader': item['uploader'],
-                    'votes': item['votes']}}
+    if user in item['voters']:
+        css_classes = get_css_classes(user, item['voters'][user])
+    else:
+        css_classes = get_css_classes(user)
+
+    return {'item': {'title': item['title'],
+                     'image': b64(item['image']),
+                     'id': item_id,
+                     'uploader_name': root['users'][item['uploader']]['name'],
+                     'uploader_id': item['uploader'],
+                     'votes': votes_presage(item['votes']),
+                     'desc': item['description'],
+                     'tags': item['tags'],
+                     'css_classes': css_classes}}
+
+
+@view_config(route_name='full_image', renderer='../templates/full_image.pt')
+def full_image_view(request):
+    root = firstpyramid.root_factory(request)
+    item_id = request.url.split('/')[-1]
+    item = root['images'].get(int(item_id))
+    return{'image': b64(item['image'])}
 
 
 @view_config(route_name='upvote', renderer='json')
 def upvote_data(request):
     image_id = int(request.path.split('/')[-1])
-    votes = ImageItem().update_votes(image_id, 1, request)
-    return{'votes': votes_presage(votes)}
+    if ImageItem().exists(request, image_id):
+        votes = ImageItem().update_votes(image_id, 1, request)
+        return{'votes': votes_presage(votes)}
+    else:
+        return 'The requested image does not exist'
 
 
 @view_config(route_name='downvote', renderer='json')
@@ -112,3 +134,21 @@ def votes_presage(votes):
         # Remove "-" from number
         votes = -votes
     return pre + str(votes)
+
+
+def get_css_classes(userid, vote=''):
+    base_classes = ['btn', 'btn-lg', 'btn-default', 'vote-button']
+
+    if vote or not userid:
+        base_classes.append('disabled')
+
+    up_classes = base_classes + ['btn-success']
+    down_classes = base_classes + ['btn-warning']
+
+    if vote == 'up':
+        up_classes.append('voted')
+    elif vote == 'down':
+        down_classes.append('voted')
+
+    return {'up_classes': ' '.join(up_classes),
+            'down_classes': ' '.join(down_classes)}
